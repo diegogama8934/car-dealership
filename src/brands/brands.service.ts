601658827ee1b4134b0a1c18e3e847c608fa6625
Diego Martinez Garcia <diegogama8934@gmail.com>
2024-12-30 16:24:29 -0600

    ADD: seed route

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
 import { Brand } from './entities/brand.entity';
